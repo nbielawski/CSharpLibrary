d9ef27833ff9f70d8a0586aa124b96378e8e32f1
Nick Bielawski <nbielawski12@gmail.com>
2017-10-25 17:00:12 -0400

    Added numerous CSharp files

---
+++ 0.06_ForLoops/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._06_ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //for (int i = 0; i <= 10; i++)
+            //{
+
+            //    if (i < 10)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    else if (i == 10)
+            //    {
+            //        Console.WriteLine("Launch");
+            //    }
+
+
+            //}
+
+            int n = 0;
+            while (n + 5 <= 100)
+            {
+                Console.WriteLine(n += 5);
+            }
+
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ 0.07_Objects/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._07_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Donut jellyDonut = new Donut();
+            Donut kennDonut = new Donut();
+            Donut nickDonut = new Donut();
+
+            jellyDonut.Filling = "Cherry Jely";
+            jellyDonut.Price = 7.99M;
+            jellyDonut.Type = "Jelly";
+            jellyDonut.IsSpecial = true;
+
+            kennDonut.Filling = "strawberry Jely";
+            kennDonut.Price = 0;
+            kennDonut.Type = "Jelly";
+            kennDonut.IsSpecial = false;
+
+            nickDonut.Filling = "chocolate";
+            nickDonut.Price = 1.99M;
+            nickDonut.Type = "Awesome";
+            nickDonut.IsSpecial = true;
+
+            Console.WriteLine(jellyDonut);
+            Console.Read();
+        }
+    }
+}
